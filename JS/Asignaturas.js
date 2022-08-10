d2f99ac7895c557c1fb4024926ffc20c1b35889e
bayronmeraz <bayronmeraz@unah.hn>
2022-08-10 14:00:32 -0600

    Agregue get asignatura y actualizar asignatura

--- JS/Asignaturas.js
+++ JS/Asignaturas.js
@@ -1,5 +1,7 @@
 var UrlAsignaturas = 'http://20.216.41.245:90/G7_20/controller/AsignaturaControlle.php?opc=Getasignaturas';
 var UrlInsertAsignaturas = 'http://20.216.41.245:90/G7_20/controller/AsignaturaControlle.php?opc=insertasignatura';
+var UrlGetAsignatura = 'http://20.216.41.245:90/G7_20/controller/AsignaturaControlle.php?opc=Getasignatura';
+var UrlUpdateAsignatura = 'http://20.216.41.245:90/G7_20/controller/AsignaturaControlle.php?opc=Updateasignatura';
 
 $(document).ready(function(){
   CargarAsignaturas();
@@ -23,6 +25,9 @@ function CargarAsignaturas(){
                 '<td>'+ MiItems[i].UnidadesValorativas +'</td>'+
                 '<td>'+ MiItems[i].PromedioAprobacion +'</td>'+
                 '<td>'+ MiItems[i].NumeroEdificio +'</td>'+
+                '<td>'+
+                '<button class = "btn btn-info" onclick = "CargarAsignatura('+ MiItems[i].CodigoAsignatura+')">Editar</button>'+
+                '</td>'+
             '</tr>';
             $('#DataAsignaturas').html(Valores);
             }
@@ -35,7 +40,7 @@ function AgregarAsignatura(){
     CodigoAsignatura : $('#CodigoAsignatura').val(),
     NombreAsignatura : $('#NombreAsignatura').val(),
     Carrera : $('#Carrera').val(),
-    FechaCreacion : $('#FechaCreacion').val(),
+    FechaCreacion: $('#FechaCreacion').val(),
     UnidadesValorativas : $('#UnidadesValorativas').val(),
     PromedioAprobacion : $('#PromedioAprobacion').val(),
     NumeroEdificio : $('#NumeroEdificio').val()
@@ -54,6 +59,63 @@ function AgregarAsignatura(){
         },
         Error:function(textStatus, errorThrown){
             alert('Error al agregar asignatura'+ textStatus + errorThrown);
+        }
+    });
+    alert('Aviso');
+}
+
+function CargarAsignatura (CodAsignatura){
+    var datosasignatura = {
+        CodigoAsignatura: CodAsignatura
+    };
+    var datosasignaturajson = JSON.stringify(datosasignatura);
+
+    $.ajax({
+        url: UrlGetAsignatura,
+        type: 'POST',
+        data: datosasignaturajson,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function(response){
+            var MiItems = response;
+            $('#CodigoAsignatura').val(MiItems[0].CodigoAsignatura);
+            $('#NombreAsignatura').val(MiItems[0].NombreAsignatura);
+            $('#Carrera').val(MiItems[0].Carrera);
+            $('#FechaCreacion').val(MiItems[0].FechaCreacion);
+            $('#UnidadesValorativas').val(MiItems[0].UnidadesValorativas);
+            $('#PromedioAprobacion').val(MiItems[0].PromedioAprobacion);
+            $('#NumeroEdificio').val(MiItems[0].NumeroEdificio);
+            var btnactualizar = '<input type = "submit" id= "btn_actualizar" onclick = "ActualizarAsignatura('+ MiItems[0].CodigoAsignatura +')"'+
+            'value="Actualizar Asignatura" class = "btn btn-primary"></tr>';
+            $('#btnagregarasignatura').html(btnactualizar);
+        }
+
+    });
+}
+
+function ActualizarAsignatura(CodAsignatura){
+    var datosasignatura = {
+        CodigoAsignatura: CodAsignatura,
+        NombreAsignatura: $('#NombreAsignatura').val(),
+        Carrera: $('#Carrera').val(),
+        FechaCreacion: $('#FechaCreacion').val(),
+        UnidadesValorativas: $('#UnidadesValorativas').val(),
+        PromedioAprobacion: $('#PromedioAprobacion').val(),
+        NumeroEdificio: $('#NumeroEdificio').val()   
+    };
+    var datosasignaturajson= JSON.stringify(datosasignatura);
+    $.ajax({
+        url: UrlUpdateAsignatura,
+        type:'PUT',
+        data: datosasignaturajson,
+        dataType: 'JSON',
+        contentType: 'application/json',
+        success: function(reponse){
+            console.log(reponse);
+            alert("Asignatura Actualizada con Exito");
+        },
+        error: function(textStatus, errorThrown){
+            alert('Error al actualizar la asignatura'+ textStatus + errorThrown);
         }
     });
     alert('Aviso');
